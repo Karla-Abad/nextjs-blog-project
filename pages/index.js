f9a58f9c631f953eb991af63f6bef80c8322c526
Karla Abad <kabad1014@gmail.com>
2023-04-20 13:05:35 -0400

    Configure notification component.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import Head from "next/head";
 import Hero from "../components/home-page/Hero";
 import FeaturedPosts from "../components/home-page/FeaturedPosts";
 import { getFeaturedPosts } from "../lib/posts-util";
@@ -6,6 +7,13 @@ import { getFeaturedPosts } from "../lib/posts-util";
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Karla's Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
